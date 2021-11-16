63e454ab8196d6bf78327273f8354b4d211135ce
Inon-Sinn <inon.sinn@gmail.com>
2021-11-16 13:14:44 +0200

    finished Calls_to_csv

--- Classes/CallforElevator.py
+++ Classes/CallforElevator.py
@@ -1,11 +1,17 @@
 class CallforElevator:
-    def __init__(self, state, time, src, dest, type, allocated_to):
-        self.state = state
-        self.time = time
-        self.src = src
-        self.dest = dest
-        self.type = type
-        self.allocated_to = allocated_to
+
+    #INIT=0, GOING2SRC=1, GOIND2DEST=2, DONE=3; - state
+    #UP=1, DOWN=-1; - type
+    def __init__(self, time, src, dest, state, allocated_to):
+        self.state = int(state)
+        self.time = float(time)
+        self.src = int(src)
+        self.dest = int(dest)
+        if self.dest - self.src > 0:
+            self.type = 1
+        else:
+            self.type = -1
+        self.allocated_to = int(allocated_to)
 
     def getState(self):
         return self.state
@@ -24,3 +30,9 @@ class CallforElevator:
 
     def getAllocation(self):
         return self.allocated_to
+
+    def __str__(self):
+        return "state:{state} time:{time} src:{src} dest:{dest} type:{type} acllocated_to:{allo}\n".format(state=self.state,time=self.time,src=self.src,dest=self.dest,type=self.type,allo=self.allocated_to)
+
+    def __repr__(self):
+        return "state:{state} time:{time} src:{src} dest:{dest} type:{type} acllocated_to:{allo}\n".format(state=self.state,time=self.time,src=self.src,dest=self.dest,type=self.type,allo=self.allocated_to)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import csv
 from Classes import Building as bld
 from Classes import Elevator as elv
 from Classes import CallforElevator as call
@@ -29,19 +30,35 @@ def load_json_building(file_name) -> bld.Building:
 #         print(err)
 
 
-# def load_csv_call(self, file_name):
-#     try:
-#         with open(file_name, "r+") as f:
-#             my_d = json.load(f)
-#             c = call.CallforElevator(my_d["_state"], my_d["_time"], my_d["_src"], my_d["_dest"],
-#                                      my_d["_type"], my_d["_allocated_to"])
-#     except IOError as err:
-#         print(err)
+def load_csv_call(file_name):
+    calls = []
+    with open(file_name) as f:
+        csvreader = csv.reader(f)
+        for row in csvreader:
+            c = call.CallforElevator(time=row[1], src=row[2], dest=row[3], state=row[4], allocated_to=row[5])
+            calls.append(c)
+    return calls
+
+
+def call_to_csv(call_list, file_name):
+    rows = []
+    for c in call_list:
+        rows.append(["Elevator call", c.time, c.src, c.dest, c.state, c.allocated_to])
+    with open(file_name, 'w', newline="") as f:
+        cswriter = csv.writer(f)
+        cswriter.writerows(rows)
+
 
 if __name__ == '__main__':
-    path = "Ex1_input/Ex1_Buildings/B1.json"
-    MyBuilding = load_json_building(path)
-    print(MyBuilding)
-    num = MyBuilding.getNumofElevators()
-    for i in range(MyBuilding.getNumofElevators()):
-        print(MyBuilding.getElevetor(i),"\n")
+    path_Json = "Ex1_input/Ex1_Buildings/B1.json"
+    path_csv = "Ex1_input/Ex1_Calls/Calls_a.csv"
+
+    MyBuilding = load_json_building(path_Json)
+    # print(MyBuilding)
+    # num = MyBuilding.getNumofElevators()
+    # for i in range(MyBuilding.getNumofElevators()):
+    #     print(MyBuilding.getElevetor(i),"\n")
+
+    MyCsv = load_csv_call(path_csv)
+    # print(MyCsv)
+    call_to_csv(MyCsv, 'newCsv2.csv')
