ede24a02a8020f26956f1ad4ff6b4b67cd586d38
Inon-Sinn <inon.sinn@gmail.com>
2021-11-18 14:09:21 +0200

    Almost Finished add_call_time

--- Classes/Allocation.py
+++ Classes/Allocation.py
@@ -2,18 +2,30 @@ from Classes.CallforElevator import CallforElevator as cl
 from Classes.Block import Block as blk
 
 
-def add(call: cl, block_list: list):
-    c_place = add_call_time(call.time, block_list)
-    src_place = add_stop(call.src, c_place, block_list, False)
-    dest_place = add_stop(call.dest, src_place, block_list, True)
+def add(call: cl, block_list: list, speed):
+    c_place = add_call_time(call.time, block_list, speed)
+    src_place = add_stop(call.src, c_place, block_list, False, speed)
+    dest_place = add_stop(call.dest, src_place, block_list, True, speed)
     add_people(c_place, dest_place, block_list)
 
 
-def add_call_time(time, block_list):
+def add_call_time(time, block_list, speed):  # UNFINISHED
+    last = len(block_list) - 1
+    if block_list[last].time <= time:  # Adding a new block to the end the list
+        new_block = blk(block_list[last - 1].type, time, block_list[last - 1].place, block_list[last - 1].people + 1)
+        block_list.append(new_block)
+        return last + 1
+
+    for i in range(len(block_list) - 1):  # Inserting the block somewhere in between
+        if block_list[i].time <= time <= block_list[i + 1]:
+            new_place = 0  # block_list[i].place + (time-block_list[i].time) UNFINISHED
+            new_block2 = blk(block_list[i].type, time, new_place, block_list[i].people + 1)
+            block_list.insert(i + 1, new_block2)
+            return i + 1
     return 0
 
 
-def add_stop(place, last_place, block_list, dest_bool):
+def add_stop(place, last_place, block_list, dest_bool, speed):  # UNFINISHED
     return 0
 
 
@@ -60,16 +72,16 @@ class Allocation:
         temporary_total_time_List = list()
         for elev in range(len(self.total_List)):  # goes to over the elvetors for the calculation
             list_copy = self.total_List[elev].copy()
-            add(call, list_copy)
+            add(call, list_copy, self.building.getElevetor(elev).getSpeed)
             total_time = total_time_calculator(list_copy)
             temporary_total_time_List.append(total_time - self.total_time_list[elev])
-        min = temporary_total_time_List[0]  # Calculate which is the best choose
+        minimum = temporary_total_time_List[0]  # Calculate which is the best choose
         ans = 0
         for i in range(len(temporary_total_time_List)):
-            if min > temporary_total_time_List[i]:
-                min = temporary_total_time_List[i]
+            if minimum > temporary_total_time_List[i]:
+                minimum = temporary_total_time_List[i]
                 ans = i
-        self.total_time_list[ans] += min  # Update the new choose
-        self.update_total_time(min)
-        add(call, self.total_List[ans])
+        self.total_time_list[ans] += minimum  # Update the new choose
+        self.update_total_time(minimum)
+        add(call, self.total_List[ans], self.building.getElevetor(ans).getSpeed)
         return ans
